dd7766caffb6c8c8b3989d9a9baf6d3408ff6cc8
domivbhh <hariharanvbit123@gmail.com>
2024-05-04 09:44:01 +0530

    header modified

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {useSelector} from 'react-redux';
+import {FaSearch} from 'react-icons/fa';
 
 const Header = () => {
   const currentUser=useSelector((state)=>state.user)
@@ -9,7 +10,14 @@ const Header = () => {
   return (
     <div className="bg-slate-200 ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <h1 className="font-bold">Auth App</h1>
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span className='text-slate-500'>VB</span>
+          <span className='text-slate-500'>-Estate</span>
+        </h1>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+          <input type="text" placeholder='search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+          <FaSearch className='text-slate-500'/>
+        </form>
         <ul className="flex gap-4">
           <Link to="/">
             <li>Home</li>
