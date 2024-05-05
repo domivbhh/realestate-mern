bf880c35e1cce3d4de8484befbd3b9f6668e5d5e
domivbhh <hariharanvbit123@gmail.com>
2024-05-05 16:05:56 +0530

    listing page with image slider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,24 +9,31 @@ import PrivateRoute from "./components/PrivateRoute";
 import Login from "./pages/Login";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
 
 
 function App() {
-  return <BrowserRouter>
-  <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route element={<PrivateRoute/>}>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/create-listing" element={<CreateListing/>}/>
-        <Route path="/update-listing/:listingId" element={<UpdateListing/>}/>
-      </Route>
-      <Route path="/signin" element={<Login/>}/>
-      <Route path="/signup" element={<Signup/>}/>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
 
-    </Routes>
-  </BrowserRouter>
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
+        </Route>
+        <Route path="/signin" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 
 
     
