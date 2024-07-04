32b43fed01a4ecbea9b6cfb9ff9c70f022e0cc55
hariharan <154161523+domivbhh@users.noreply.github.com>
2024-07-04 13:20:13 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
 app.use('/api/listings',listingRoutes)
 
-app.use(express.static(path.join(__dirname,'/client/dist')))
+// app.use(express.static(path.join(__dirname,'/client/dist')))
 
 // app.get('*',(req,res)=>{
 //     res.sendFile(path.join(__dirname,'client','dist','index.html'))
